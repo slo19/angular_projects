f9b3630955eaf733e86595924ee796a6f656341c
saulo-miranda <saulo.miranda@ufv.br>
2024-02-24 17:12:59 -0300

    validation added

--- issue-tracker/src/app/issue-report/issue-report.component.ts
+++ issue-tracker/src/app/issue-report/issue-report.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, inject, Output, } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { Issue } from '../issue';
 import { IssuesService } from '../issues.service';
@@ -19,15 +19,19 @@ export class IssueReportComponent {
   @Output() formClose = new EventEmitter();
 
   private readonly issuesService = inject(IssuesService);
-  
+
   issueForm = new FormGroup<IssueForm>({
-    title: new FormControl('', { nonNullable: true}),
-    description: new FormControl('', { nonNullable: true}),
-    priority: new FormControl('', { nonNullable: true }),
-    type: new FormControl('', { nonNullable: true})
+    title: new FormControl('', { nonNullable: true, validators: Validators.required }),
+    description: new FormControl('', { nonNullable: true, validators: Validators.required }),
+    priority: new FormControl('', { nonNullable: true, validators: Validators.required }),
+    type: new FormControl('', { nonNullable: true, validators: Validators.required })
   });
 
   addIssue() {
+    if (this.issueForm && this.issueForm.invalid) {
+      this.issueForm.markAllAsTouched();
+      return;
+    }
     this.issuesService.createIssue(this.issueForm.getRawValue() as Issue);
     this.formClose.emit();
   }
