9d7d93d5e3849f6a0d3bf5083e3f121fcda9076e
saulo-miranda <saulo.miranda@ufv.br>
2024-02-24 18:00:39 -0300

    feature: remove issue

--- issue-tracker/src/app/issue-list/issue-list.component.ts
+++ issue-tracker/src/app/issue-list/issue-list.component.ts
@@ -11,10 +11,14 @@ export class IssueListComponent implements OnInit {
   private readonly issuesService = inject(IssuesService);
   showReportIssue = false;
   issues: Issue[] = [];
+  selectedIssue: Issue | null = null;
+
   constructor() {}
+
   ngOnInit(): void {
     this.getIssues();
   }
+
   private getIssues() {
     this.issues = this.issuesService.getPendingIssues();
   }
@@ -22,5 +26,18 @@ export class IssueListComponent implements OnInit {
   onCloseReport() {
     this.showReportIssue = false;
     this.getIssues();
+  }
+
+  onConfirm(confirmed: boolean) {
+    if (confirmed && this.selectedIssue) {
+      this.issuesService.completeIssue(this.selectedIssue);
+      this.getIssues();
+    }
+    this.selectedIssue = null;
+  }
+
+  select(issue: Issue) {
+    this.selectedIssue = issue;
+    console.log(this.selectedIssue);
   }
 }
