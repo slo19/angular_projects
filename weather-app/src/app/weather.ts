0ac6571ae5ff06fbb5ef2966355c87732adcb52c
saulo-miranda <saulo.miranda@ufv.br>
2024-03-02 23:07:35 -0300

    weather model added

---
+++ weather-app/src/app/weather.ts
@@ -0,0 +1,48 @@
+export interface Weather {
+  weather: WeatherInfo[];
+  coord: Coordinate;
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+    sea_level: number;
+    grnd_level: number;
+  };
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
+
+interface WeatherInfo {
+  main: string;
+  icon: string;
+  description: string;
+  id: number;
+}
+
+interface Coordinate {
+  lon: number;
+  lat: number;
+}
