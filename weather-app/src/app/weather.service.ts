84c4385b4c08b1ea016be405c45979f29a1ef7ea
saulo-miranda <saulo.miranda@ufv.br>
2024-03-02 23:24:37 -0300

    service added

--- weather-app/src/app/weather.service.ts
+++ weather-app/src/app/weather.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Weather } from './weather';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +11,12 @@ export class WeatherService {
   private apiUrl = 'https://api.openweathermap.org/data/2.5/';
   private apiKey = '';
   constructor() { }
+
+  getWeather(city: string): Observable<Weather> {
+    const options = new HttpParams()
+      .set('units', 'metric')
+      .set('q', city)
+      .set('appId', this.apiKey);
+    return this.http.get<Weather>(this.apiUrl + weather, {params: options});
+  }
 }
