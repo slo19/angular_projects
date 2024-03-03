6e810eda5be3ac3276cd018414e80d5afec8aa86
saulo-miranda <saulo.miranda@ufv.br>
2024-03-02 23:35:29 -0300

    added component

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -4,14 +4,22 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { WeatherComponent } from './weather/weather.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatIconModule,
+    MatInputModule,
+    MatCardModule
   ],
   providers: [
     provideAnimationsAsync()
---
+++ weather-app/src/app/weather/weather.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Weather } from '../weather';
+import { WeatherService } '../weather.service';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrl: './weather.component.scss'
+})
+export class WeatherComponent {
+  weather: Weather | undefined;
+  constructor(private weatherService: WeatherService){}
+
+  search(city: string) {
+    this.weatherService
+      .getWeather(city)
+      .subscribe(weather => this.weather = weather);
+  }
+}
